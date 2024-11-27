38dc16db670d4433f71f17f63c631cde264130d8
VITALIY1986 <staslego1986@gmail.com>
2024-11-27 17:24:32 +0200

    sadggadsadgs

--- src/components/richtext/RichTextRenderer.tsx
+++ src/components/richtext/RichTextRenderer.tsx
@@ -126,11 +126,12 @@ const RichTextRenderer: React.FC<RichTextProps> = ({ content, links }) => {
           <Image
             src={asset.url}
             alt={asset.title || 'Embedded asset'}
-            width={asset.width || 800}
+            width={asset.width || 600}
             height={asset.height || 600}
             layout="responsive"
             priority
-           
+            sizes="(max-width: 600px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            className='min-h-15 overflow-hidden'
           />
         );
       },
