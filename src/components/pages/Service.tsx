94bb4d590273be24387d9876a901634c47305860
VITALIY1986 <staslego1986@gmail.com>
2024-12-05 18:43:24 +0200

    aaaa

--- src/components/pages/Service.tsx
+++ src/components/pages/Service.tsx
@@ -1,12 +1,7 @@
 import React, { FC } from "react";
 import scss from "./Style.module.scss";
 import { FormattedMessage } from "react-intl";
-import Image from "next/image";
-import TeethIcon from "@/assets/icons/Teeth.webp";
-import MedPlus from "@/assets/icons/med_plus.png";
-import CheckMark from "@/assets/icons/check_mark.png";
-import BxChild from "@/assets/icons/bx-child.png";
-import ReactYoutube from "@/components/react-youtube/ReactYoutube";
+
 import ContactForm from "@/components/contact/ContactForm";
 interface ServiceProps {
 	programaretitle: string;
