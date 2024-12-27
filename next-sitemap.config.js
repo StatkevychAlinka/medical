9d0fc48e2c7f4da58591f990e58bfaa4b42c7823
VITALIY1986 <staslego1986@gmail.com>
2024-12-27 20:33:14 +0200

    asgdgad

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,25 +1,13 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://crearesite-web.ro',
-  generateRobotsTxt: true, // Enables the generation of robots.txt
-  changefreq: 'daily', // Specifies how often the content changes
-  priority: 0.7, // Default priority for pages
-  sitemapSize: 5000, // Maximum number of entries per sitemap file
+  generateRobotsTxt: true, // Генерация robots.txt
+  changefreq: 'daily', // Частота изменений страниц
+  priority: 0.7,
+  sitemapSize: 5000,
+  // Настройка локалей для sitemap
   i18n: {
-    locales: ['en-US', 'ro-RO'], // Added 'en-US' for clarity; adjust based on your setup
-    defaultLocale: 'ro-RO', // Default locale for the sitemap
+    locales: [, 'ro-RO'],
+    defaultLocale: 'ro-RO',
   },
-  // Custom configuration for excluding specific routes or dynamic patterns
-  exclude: ['/api/*', '/admin/*'], // Exclude API and admin routes from the sitemap
-  // Additional sitemap options (e.g., alternate links for localization)
-  alternateRefs: [
-    {
-      href: 'https://crearesite-web.ro',
-      hreflang: 'ro-RO',
-    },
-    {
-      href: 'https://crearesite-web.ro/en',
-      hreflang: 'en-US',
-    },
-  ],
 };
