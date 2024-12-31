9e269c5a272a2b07598999b1ce6a30e777b707bd
VITALIY1986 <staslego1986@gmail.com>
2024-12-31 11:37:53 +0200

    asdggads

---
+++ src/pages/next-seo.config.js
@@ -0,0 +1,27 @@
+const SEO = {
+    title: "Default Title",
+    description: "Default description for the site.",
+    canonical: "https://crearesite-web.ro",
+    openGraph: {
+      type: "website",
+      locale: "en_IE",
+      url: "https://crearesite-web.ro",
+      site_name: "CreareSite",
+      images: [
+        {
+          url: "https://crearesite-web.ro/default-image.jpg",
+          width: 800,
+          height: 600,
+          alt: "Default Image Alt Text",
+        },
+      ],
+    },
+    twitter: {
+      handle: "@handle",
+      site: "@site",
+      cardType: "summary_large_image",
+    },
+  };
+  
+  export default SEO;
+  
