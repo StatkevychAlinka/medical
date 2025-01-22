e12f945d2be51e7b91a1a3fc9c6eb291b3830cf3
VITALIY1986 <staslego1986@gmail.com>
2025-01-22 14:27:21 +0200

    agsdads

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,30 +2,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html >
+    <Html>
       <Head>
         {/* PWA Meta Tags */}
-		<link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
         <link rel="icon" type="image/png" sizes="192x192" href="/icons/icon-192x192.png" />
         <link rel="apple-touch-icon" sizes="512x512" href="/icons/icon-512x512.png" />
-    
-        {/* Add a service worker */}
-        <script>
-          {`
-            if ('serviceWorker' in navigator) {
-              window.addEventListener('load', () => {
-                navigator.serviceWorker.register('/service-worker.js')
-                  .then(registration => {
-                    console.log('Service Worker registered with scope:', registration.scope);
-                  })
-                  .catch(error => {
-                    console.log('Service Worker registration failed:', error);
-                  });
-              });
-            }
-          `}
-        </script>
       </Head>
       <body>
         <Main />
